075d2f75a4adf856c33cab2d81c2810387cd4ce5
RafatAiub <tanvirmahtab1996@gmail.com>
2022-01-12 08:13:33 +0600

    rdms using oracle database

---
+++ admin/add_customer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <body>
+	<form method="post" action="input.php">
+		First name:<br>
+		<input type="text" name="cname">
+		<br>
+		Last name:<br>
+		<input type="text" name="email">
+		<br>
+		Password<br>
+		<input type="text" name="pass">
+		<br>
+		
+		Phone Number:<br>
+		<input type="text" name="phone">
+		<br>
+		City name:<br>
+		<input type="text" name="address">
+		<br>
+		
+		<input type="submit" name="save" value="submit">
+	</form>
+  </body>
+</html>
